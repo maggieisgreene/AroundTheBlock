5b6cfb23f5d15b053342a9c0edc400b9d555ce42
unknown <maggie.greene96@gmail.com>
2020-03-31 15:03:58 -0500

    Work in progress

---
+++ AroundTheBlock/Controllers/DogController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+using AroundTheBlock.Models;
+
+namespace AroundTheBlock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DogController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // Get all dogs from the database
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name, OwnerId, Breed FROM Dog";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Dog> dogs = new List<Dog>();
+
+                    while (reader.Read())
+                    {
+                        Dog dog = new Dog
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed"))
+                        };
+
+                        dogs.Add(dog);
+                    }
+                    reader.Close();
+
+                    return Ok(dogs);
+                }
+            }
+        }
+    }
+}
